543523ba3e868e9acec59a42fc87a83105d41851
KeiichiHonma <keiichi1125@gmail.com>
2020-04-10 16:31:36 +0900

    1

--- src/main.js
+++ src/main.js
@@ -1,8 +1,34 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import router from './router'
+import firebase from 'firebase'
+
 Vue.config.productionTip = false
 
-new Vue({
-  render: h => h(App),
-}).$mount('#app')
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyAbcABbJmRSrNg_NzBlvKoAAEbM6JCgF94",
+  authDomain: "trustfood-account-development.firebaseapp.com",
+  databaseURL: "https://trustfood-account-development.firebaseio.com",
+  projectId: "trustfood-account-development",
+  storageBucket: "trustfood-account-development.appspot.com",
+  messagingSenderId: "853232185583",
+  appId: "1:853232185583:web:372eb68cfda3f9c76fab1a",
+  measurementId: "G-SM5BEDL5XK"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+let app
+
+// eslint-disable-next-line no-unused-vars
+firebase.auth().onAuthStateChanged(user => {
+  if (!app) {
+    new Vue({
+      router,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+})
