4fa55fe5d550fb4cc5cc28f76003929d530afb97
keiichi-honma <keiichi1125@gmail.com>
2020-04-15 14:23:18 +0900

    2foods-account-development こちらのfireabseで再設定

--- src/main.js
+++ src/main.js
@@ -8,18 +8,19 @@ Vue.config.productionTip = false
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: "AIzaSyAbcABbJmRSrNg_NzBlvKoAAEbM6JCgF94",
-  authDomain: "trustfood-account-development.firebaseapp.com",
-  databaseURL: "https://trustfood-account-development.firebaseio.com",
-  projectId: "trustfood-account-development",
-  storageBucket: "trustfood-account-development.appspot.com",
-  messagingSenderId: "853232185583",
-  appId: "1:853232185583:web:372eb68cfda3f9c76fab1a",
-  measurementId: "G-SM5BEDL5XK"
+  apiKey: "AIzaSyDCqX5wNywVb6wTUB-O_2YEfDHeYt_PoT4",
+  authDomain: "twocloud-peppar-development.firebaseapp.com",
+  databaseURL: "https://twocloud-peppar-development.firebaseio.com",
+  projectId: "twocloud-peppar-development",
+  storageBucket: "twocloud-peppar-development.appspot.com",
+  messagingSenderId: "1036391910028",
+  appId: "1:1036391910028:web:bcc59a75b5c91ab9aa49a9",
+  measurementId: "G-S83F6QBPXL"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
+
 
 let app
 
